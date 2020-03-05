e5ac2a87ba60f261c502df82914a1ef18a3fcec3
Yannis Prekezes <ioannis.x.prekezes@aib.ie>
2020-03-05 18:39:16 +0000

    Structure reorganization

--- env-wiring/configuration-properties/src/main/java/com/foo/config/ConfigDemoProperties.java
+++ env-wiring/configuration-properties/src/main/java/com/foo/config/ConfigDemoProperties.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
