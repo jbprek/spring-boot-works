e5c04fba0cb0a7614a57d5ad229141bf43a12145
Ioannis Prekezes <jbprek@gmail.com>
2021-03-17 12:47:35 +0000

    Employees Data Rest React Client Example

---
+++ mvc-rest/employess-data-rest-react-client/src/main/java/com/foo/employee/ui/EmployessDataRestReactClientApplication.java
@@ -0,0 +1,13 @@
+package com.foo.employee.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployessDataRestReactClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployessDataRestReactClientApplication.class, args);
+	}
+
+}
